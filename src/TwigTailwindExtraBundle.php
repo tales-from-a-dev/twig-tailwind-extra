1a8a53d845a04021f0d4f38b65d3e6cf4298ef3d
Romain Monteil <monteil.romain@gmail.com>
2023-11-17 17:52:33 +0100

    :bug: Replace Bundle with AbstractBundle

--- src/TwigTailwindExtraBundle.php
+++ src/TwigTailwindExtraBundle.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace TalesFromADev\Twig\Extra\Tailwind;
 
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use TalesFromADev\Twig\Extra\Tailwind\Bundle\DependencyInjection\TwigTailwindExtraExtension;
 
-final class TwigTailwindExtraBundle extends Bundle
+final class TwigTailwindExtraBundle extends AbstractBundle
 {
     public function getContainerExtension(): ?ExtensionInterface
     {
