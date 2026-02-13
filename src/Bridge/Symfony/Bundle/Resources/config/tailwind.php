eeef2559542045c090eac3aa722b7320412718da
Hugo Alliaume <hugo@alliau.me>
2026-02-13 16:25:00 +0100

    Improve performances for multiple identical calls to `merge()`, by using `ArrayAdapter` cache

--- src/Bridge/Symfony/Bundle/Resources/config/tailwind.php
+++ src/Bridge/Symfony/Bundle/Resources/config/tailwind.php
@@ -2,11 +2,14 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\ChainAdapter;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TalesFromADev\Twig\Extra\Tailwind\TailwindExtension;
 use TalesFromADev\Twig\Extra\Tailwind\TailwindRuntime;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\abstract_arg;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\inline_service;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container) {
@@ -15,13 +18,20 @@ return static function (ContainerConfigurator $container) {
             ->parent('cache.app')
             ->tag('cache.pool', ['name' => 'twig.cache'])
 
+        ->set('twig.cache.tailwind.chain')
+            ->class(ChainAdapter::class)
+            ->args([[
+                inline_service(ArrayAdapter::class),
+                service('twig.cache.tailwind'),
+            ]])
+
         ->set('twig.extension.tailwind', TailwindExtension::class)
             ->tag('twig.extension')
 
         ->set('twig.runtime.tailwind', TailwindRuntime::class)
             ->args([
                 abstract_arg('additional configuration, set in TalesFromADevTwigExtraTailwindExtension'),
-                service('twig.cache.tailwind'),
+                service('twig.cache.tailwind.chain'),
             ])
             ->tag('twig.runtime')
     ;
